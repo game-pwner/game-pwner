2ba21c1cd38ac191ed0e011be2651811e5f9aa63
Ivan Stepanov <ivanstepanovftw@gmail.com>
2019-05-20 12:27:03 +0300

    tool/gui: not working

--- tools/gui/widgets/MainWindow.py
+++ tools/gui/widgets/MainWindow.py
@@ -20,26 +20,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # Windows
         self.window_about = About(self)
 
-        # Tabs
+        # DockWidget
         self.dock1 = Dashboard(self)
         self.dock2 = PointerScanner(self)
 
-        # self.h_layout = QHBoxLayout(self)
-        # self.h_layout.addWidget(self.dock1)
-        # self.h_layout.addWidget(self.dock2)
-        # self.h_layout.setParent(self.layout())
-        # self.setLayout(self.layout())
-        # self.setCentralWidget(self.h_layout)
-
-        ################
-        # self.dock1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.addDockWidget(Qt.TopDockWidgetArea, self.dock1)
         self.addDockWidget(Qt.TopDockWidgetArea, self.dock2)
         self.tabifyDockWidget(self.dock1, self.dock2)
-        # self.setCorner(Qt.TopLeftCorner, Qt.LeftDockWidgetArea)
-        # self.setCorner(Qt.BottomLeftCorner, Qt.LeftDockWidgetArea)
 
     @pyqtSlot()
-    def toggle_window_about(self):
+    def onActionAbout(self):
         if self.window_about.isHidden():
             self.window_about.show()
+
+    @pyqtSlot()
+    def onActionReset(self):
+        self.dock1.show()
+        self.dock2.show()
+        self.tabifyDockWidget(self.dock1, self.dock2)
